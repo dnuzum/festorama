e7e7dec7034754ea3b7650606c7779ab1c02f33d
Derek Nuzum <derek.nuzum@gmail.com>
2016-03-24 13:19:03 -0700

    removed alerts

--- public/app/services.js
+++ public/app/services.js
@@ -32,22 +32,3 @@ angular.module('FestivalServices', ['ngResource'])
     }
   }
 }])
-
-.factory('Alerts', [function() {
-  var alerts = [];
-
-  return {
-    clear: function() {
-      alerts = [];
-    },
-    add: function(type, msg) {
-      alerts.push({type: type, msg: msg});
-    },
-    get: function() {
-      return alerts;
-    },
-    remove: function(idx) {
-      alerts.splice(idx, 1);
-    }
-  }
-}])
